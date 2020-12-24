fad767379f2474e0c7bf7abd9271b5ec506847cb
gll <guilherme.lopes123@hotmail.com>
2020-12-24 14:44:07 -0300

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Header from './components/header/Header' 
-
+import Header from './components/header/Header';
 
 function App() {
   return (
+    <>
     <Header></Header>
+    <div className="wrapper">
+      conteudo
+    </div>
+    </>
   );
 }
 
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -3,6 +3,18 @@ import './header.css';
 
 export default function header() {
   return (
-    <h1>aaa</h1>
+    <header>
+      <p>Gerador numeros loterias</p>
+      <nav>
+        <ul>
+          <li><button className='megasena'>megasena</button></li>
+          <li><button className='lotofacil'>lotofacil</button></li>
+          <li><button className='quina'>quina</button></li>
+          <li><button className='lotomania'>lotomania</button></li>
+          <li><button className='duplasena'>duplasena</button></li>
+          <li><button className='loteca'>loteca</button></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
